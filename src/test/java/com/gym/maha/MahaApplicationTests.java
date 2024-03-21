e0ef137f3cad00fcd8e07e3b73ad11c9f5fd5f12
Manoj Krishna <manoj.dhayalan@mapp.com>
2024-03-21 11:03:44 +0100

    first commit

---
+++ src/test/java/com/gym/maha/MahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.maha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
